e311102a31c6abf4e8ce9531a3e7ece5351dd12f
Rusty Bailey <rustylbailey@gmail.com>
2021-01-01 16:17:25 -0500

    Create first draft of credits scene

---
+++ Assets/Scripts/SceneMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMusic : MonoBehaviour
+{
+    [SerializeField] string musicToPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!string.IsNullOrEmpty(musicToPlay))
+        {
+            AudioManager.instance.PlayMusic(musicToPlay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
