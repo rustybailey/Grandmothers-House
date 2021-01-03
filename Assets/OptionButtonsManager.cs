d77328828a80942ee272ef31454b3d097132c91f
Rusty Bailey <rustylbailey@gmail.com>
2021-01-03 00:20:38 -0500

    Navigate purely with keyboard

---
+++ Assets/OptionButtonsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionButtonsManager : MonoBehaviour
+{
+    public void SelectFirstActiveOptionButton()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            if (gameObject.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                gameObject.transform.GetChild(i).GetComponent<Button>().Select();
+                return;
+            }
+        }
+    }
+}
